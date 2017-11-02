ecb3d248f4501583285f97d2f22e0cb179de6dfe
陈治文 <cheng022074@qq.com>
2017-11-02 11:55:32 +0800

    no message

--- src/code.js
+++ src/code.js
@@ -1,5 +1,7 @@
 const {
-    file:is_file
+    file:is_file,
+    string:is_string,
+    simpleObject:is_simple_object
 } = require('./is'),
 {
     readTextFile
@@ -71,6 +73,59 @@ class BinCode extends Code{
 
 exports.BinCode = BinCode ;
 
+const textCodeMetaRe = /^\/\*(?:.|[^.])+?\*\//;
+
+function get_text_code_params(meta){
+    
+    let textCodeMetaParamRe = /@param\s+\{([^\{\}]+)\}\s+([^\s\n\r]+)\s+([^\n\r]+)/g,
+        match,
+        params = [];
+
+    while(match = textCodeMetaParamRe.exec(code)){
+        
+        params.push({
+            type:match[1].trim(),
+            name:match[2].trim()
+        }) ;
+    }
+
+    return params ;
+}
+
+function get_text_code_imports(meta){
+
+    let textCodeMetaImportRe = /@import\s+([^\n\r]+)/g,
+        textCodeMetaAliasImportRe = /(\w+)\s+from\s+(\w+(?:\.\w+)?)/ ;
+        match,
+        imports = [];
+
+    while(match = textCodeMetaImportRe.exec(code)){
+
+        let content = match[1].trim() ;
+
+        {
+            let match = content.match(textCodeMetaAliasImportRe) ;
+
+            if(match){
+
+                imports.push({
+                    var:match[1],
+                    require:match[2]
+                }) ;
+
+                continue ;
+            }
+        }
+
+        imports.push({
+            var:content,
+            require:content
+        }) ;
+    }
+
+    return imports ;
+}
+
 class SourceCode extends Code{
 
     get code(){
@@ -83,14 +138,36 @@ class SourceCode extends Code{
         }
     }
 
-    get compiledCode(){
+    get meta(){
 
-        return ;
-    }
+        let code = this.code ;
 
-    get packagedCode(){
+        if(is_string(code)){
 
-        return ;
+            let match = code.trim().match(textCodeMetaRe) ;
+
+            if(match){
+
+
+            }
+
+            return {
+                code
+            } ;
+
+        }else if(is_simple_object(code)){
+
+            let {
+                imports,
+                params
+            } = code;
+
+            return {
+                imports,
+                params,
+                code
+            }
+        }
     }
 }
 
