c65582aeb069d923954a2dc1b10d7833783653e1
陈治文 <cheng022074@qq.com>
2017-11-11 13:09:20 +0800

    no message

--- src/code/compile/from/script/class.js
+++ src/code/compile/from/script/class.js
@@ -1,29 +1,12 @@
-const {
-    apply
-} = require('../../../../template'),
-function_params = require('../../../script/function/params'),
-import_codes = require('../../../script/imports'),
-config_codes = require('../../../script/configs'),
-extend_code = require('../../../script/function/extend');
+const compile_script = require('../script');
 
 module.exports = sourceCode =>{
 
     let {
-        shortName,
-        code,
-        meta,
-    } = sourceCode,
-    {
-        imports,
-        configs,
-        extend
-    } = meta;
+        shortName
+    } = sourceCode.meta;
 
-    return apply('code.compile.script.class' , {
-        shortName,
-        code,
-        imports:import_codes(imports),
-        configs:config_codes(configs),
-        extend:extend_code(extend)
+    return compile_script(sourceCode , 'code.compile.script.class' , {
+        shortName
     }) ;
 }
