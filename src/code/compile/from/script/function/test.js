47038b09e8ad07f6ab359976ee2a875f16339308
陈治文 <cheng022074@qq.com>
2017-11-11 11:47:53 +0800

    no message

--- src/code/compile/from/script/function/test.js
+++ src/code/compile/from/script/function/test.js
@@ -1,35 +1,9 @@
-const {
-    apply
-} = require('../../../../../template'),
-function_params = require('../../../../script/function/params/object'),
-import_codes = require('../../../../script/imports'),
-require_codes = require('../../../../script/requires'),
-config_codes = require('../../../../script/configs'),
-extend_code = require('../../../../script/function/extend');
+const get_script_data = require('../../script');
 
 module.exports = sourceCode =>{
 
-    let {
-        fullName,
-        code,
-        meta,
-    } = sourceCode,
-    {
-        params,
-        imports,
-        configs,
-        extend,
-        requires
-    } = meta,
-    templateName;
-
-    return apply('code.compile.script.function.test' , {
-        fullName,
-        code,
-        params:function_params(params),
-        imports:import_codes(imports),
-        requires:require_codes(requires),
-        configs:config_codes(configs),
-        extend:extend_code(extend)
-    }) ;
+    return {
+        template:'code.compile.script.function.test',
+        ...get_script_data(sourceCode)
+    } ;
 }
