a84f6db799fb41697fccd9d5cd1d315ef4c3e171
陈治文 <cheng022074@qq.com>
2017-11-12 17:12:19 +0800

    no message

--- command/watch.js
+++ command/watch.js
@@ -1,14 +1,16 @@
 const application_watch = require('../src/application/code/watch'),
+      application = require('../src/application'),
       compile =  require('./compile');
 
 module.exports = () =>{
 
-    application_watch('command' , (type , name) =>{
+    application_watch(application.get('command') , (type , name) =>{
 
         switch(type){
 
             case 'add':
             case 'change':
+            case 'remove':
 
                 compile(name) ;
         }
