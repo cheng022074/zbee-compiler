523f11f0b6a32c5e527aadce1c8b7d4e8dbcdf59
陈治文 <cheng022074@qq.com>
2017-11-21 14:09:34 +0800

    no message

--- src/zip.js
+++ src/zip.js
@@ -5,7 +5,33 @@ exports.unzip = (sourcePath , distPath) =>{
     return decompress(sourcePath , distPath) ;
 }
 
-exports.zip = () =>{
+const archiver = require('archiver'),
+      {
+        createWriteStream
+      } = require('fs');
 
-    
+exports.zip = ({
+    files = [],
+    directories = []
+} , distPath) =>{
+
+    let archive = archiver('zip', {
+        zlib:{
+            level: 9
+        }
+    }) ;
+
+    archive.pipe(createWriteStream(distPath));
+
+    files.forEach(({
+        path,
+        name
+    }) => archive.append(createReadStream(path) , {
+        name
+    })) ;
+
+    directories.forEach(({
+        path,
+        name
+    }) => archive.directory(path , name)) ;
 }
