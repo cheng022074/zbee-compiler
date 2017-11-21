9b9f76e5b0e7c9dc12cb92d49916d3cbad635575
陈治文 <cheng022074@qq.com>
2017-11-21 11:41:59 +0800

    no message

--- command/backup.js
+++ command/backup.js
@@ -1,4 +1,17 @@
+const application = require('../src/application') ;
+
 module.exports = (name = 'default') =>{
 
-    console.log('备份项目' , name) ;
+    let backup = application.get('backup') ;
+
+    if(backup.hasOwnProperty(name)){
+
+        let config = backup[name] ;
+
+        console.log('备份设置' , config) ;
+    
+    }else{
+
+        console.log('未找到备份设置') ;
+    }
 }
