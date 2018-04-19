685c3c8996b9a69cd9d6882a5567733dcff6bac5
DESKTOP-BSUQOB6\nero.chen <cheng022074@qq.com>
2018-04-19 14:59:41 +0800

    no message

--- template/code/config.js
+++ template/code/config.js
@@ -1,6 +1,35 @@
 (name , key) =>{
 
-    let target = include(`config::${name}`) ;
+    const {
+        env
+    } = process,
+    {
+        join
+    } = require('path');
+
+    let target ;
+
+    if(env.hasOwnProperty('ZBEE-APP-BIN-PATH')){
+
+        try{
+
+            target = require(join(env['ZBEE-APP-BIN-PATH'] , 'config' , `${name}.js`)) ;
+
+        }catch(err){
+
+
+        }
+    }
+
+    if(!target){
+
+        target = include(`config::${name}`) ;
+    }
+
+    if(!target){
+
+        return ;
+    }
 
     if(key){
 
