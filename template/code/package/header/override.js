c95a0ff225fb5e1d181e0df7885568f4e04d75cc
陈治文 <cheng022074@qq.com>
2018-12-28 14:26:41 +0800

    重写方法定义

--- template/code/package/header/override.js
+++ template/code/package/header/override.js
@@ -1,6 +1,13 @@
 (() =>{
 
+    const nameRe = /^(\w+)\:{2}(.+?)$/ ;
+
     return (name , fn) =>{
+
+        if(!nameRe.test(name)){
+
+            name = `<%- data %>::${name}` ;
+        }
 
         if(typeof name === 'string' && typeof fn === 'function'){
     
