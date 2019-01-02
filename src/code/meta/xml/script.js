6e213a65141053f94a194f665dbca15b5f0eea85
陈治文 <cheng022074@qq.com>
2019-01-02 10:49:11 +0800

    清理无用代码

--- src/code/meta/xml/script.js
+++
@@ -1,9 +0,0 @@
-const Meta = require('../xml');
-
-module.exports = class extends Meta{
-
-    get imports(){
-
-        return [] ;
-    }
-}
--- src/code/meta/xml/test.js
+++
@@ -1,9 +0,0 @@
-const Meta = require('./script');
-
-module.exports = class extends Meta{
-
-    constructor(code){
-
-        super(code , 'code.generate.test') ;
-    }
-}
