50179df27f5e8f8b91e42a7afd0850b62bc2ad25
陈治文 <cheng022074@qq.com>
2019-02-04 11:23:58 +0800

    修正查看当前编译器版本号指令

--- src/command/version.js
+++ src/command/version.js
@@ -1,11 +1,4 @@
-const {
-    parse
-} = require('../xml'),
-build = require('../template/build/xml');
-
 module.exports = () =>{
 
-    console.log(build(parse('<function/>').documentElement , 'template.element.script').toString()) ;
-
-    //console.log(require('../../package.json').version) ;
+    console.log(require('../../package.json').version) ;
 }
