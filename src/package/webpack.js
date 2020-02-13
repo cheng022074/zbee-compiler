67d3ad945a374d3c4e77413c7237085542115036
陈治文 <cheng022074@qq.com>
2020-02-13 18:15:58 +0800

    暂时性存储

--- src/package/webpack.js
+++ src/package/webpack.js
@@ -5,11 +5,21 @@ const {
     SourceCode
 } = require('../code'),
 {
-    APPLICATION
-} = require('../project');
+    APPLICATION,
+    COMPILER
+} = require('../project'),
+{
+    join
+} = require('path'),
+{
+    writeTextFile
+} = require('../fs'),
+webpack = require('webpack'),
+webpackMerge = require('webpack-merge');
 
 module.exports = (codes , {
-    config
+    config,
+    webpack:webpackConfig
 }) =>{
 
     const {
@@ -38,7 +48,74 @@ module.exports = (codes , {
             config
         });
 
+    
+
+    let rootPath = join(APPLICATION.getFolderPath('bin') , Date.now().toString()),
+        entry = join(rootPath , 'src.js') ;
+
+    writeTextFile(entry , data) ;
+
+    if(webpackConfig){
+
+        if(webpackConfig === true){
+
+            webpackConfig = {} ;
+        }
+
+        webpack(webpackMerge({
+            mode:'production',
+            optimization: {
+                minimize: false
+            },
+            context:COMPILER.rootPath,
+            entry,
+            output:{
+                filename:'dist.js',
+                path:rootPath
+            },
+            resolve: {
+                modules: [
+                    join(COMPILER.rootPath , 'node_modules')
+                ]
+            },
+            target:'electron-renderer',
+            module: {
+                rules:[{
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            cacheDirectory:true,
+                            presets: [
+                                '@babel/preset-env'
+                            ],
+                            plugins: [
+                                [
+                                    '@babel/plugin-transform-runtime',
+                                    {
+                                        corejs:{
+                                            version:3,
+                                            proposals:true
+                                        }
+                                    }
+                                ]
+                            ]
+                        }
+                    }
+                }]
+            }
+        } , webpackConfig), (err, stats) => {
+    
+            if (err || stats.hasErrors()){
+    
+                console.log(err , stats.toJson().errors) ;
+            }
+        });
+    
+    }
+
     return {
         ['index.js']:data
-     } ;
+    } ;
 }
