0b4c5a09a65f8c72c2f73caad30d55aa45d1fa23
cheng022074 <cheng022074@qq.com>
2020-03-02 21:37:49 +0800

    更新

--- src/package/node.js
+++ src/package/node.js
@@ -1,57 +1,8 @@
-const {
-    apply
-} = require('../template'),
-{
-    SourceCode
-} = require('../code'),
-{
-    APPLICATION
-} = require('../project'),
-{
-    min
-} = require('../script') ;
+const webpack = require('./webpack') ;
 
-module.exports = (codes , {
-    config,
-    bootstrap,
-    main,
-    minify = false
-}) =>{
-
-    const {
-        defaultFolder
-    } = APPLICATION ;
-
-    let codeMap = {};
-
-    for(let code of codes){
-
-        let data = SourceCode.getProperty(code , 'data') ;
-
-        if(data){
-
-            let {
-                fullName
-            } = code;
-
-            codeMap[fullName] = data ;
-        }
+module.exports = (codes , config , name) => webpack(codes , {
+    ...config,
+    webpack:{
+        target:'node'
     }
-
-    let code = apply('code.package.bundle.node' , {
-        defaultFolder,
-        codeMap,
-        config,
-        bootstrap,
-        main
-    }) ;
-
-    if(minify === true){
-
-        code = min(code) ;
-    }
-
-    return {
-        ['index.js']:code
-     } ;
-}
+} , name) ;
